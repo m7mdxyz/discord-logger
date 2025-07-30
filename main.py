a69d59aeabfbf9e79a6b13e70a3ce27fa8914502
CuzImAzizx <100154535+CuzImAzizx@users.noreply.github.com>
2025-07-30 11:56:06 +0300

    Add joined guilds number validation

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ from dotenv import load_dotenv
 import os
 # ==== End of imports ====
 
-# ==== Start of logic ====
+# ==== Start of bot's logic ====
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
+        # Make sure the bot is logging one server only
+        if client.guilds.__len__() > 1:
+            print(f"The bot have joined {client.guilds.__len__()} guilds. Multiple guilds logging is not supported")
+            client.close()
+
+        # TODO: Store the guild, members, and all dat shit.
+
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-# ==== End of logic ====
+        # TODO: Store the message
+# ==== End of bot's logic ====
 
 # ==== Start of Intents, permissions, and tokens ====
 intents = discord.Intents.default()
@@ -20,5 +28,9 @@ intents.message_content = True
 client = MyClient(intents=intents)
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
-client.run(token)
 # ==== End of Intents, permissions, and tokens ====
+
+# ==== Start of main logic ====
+client.run(token)
+
+# ==== End of main logic ====
