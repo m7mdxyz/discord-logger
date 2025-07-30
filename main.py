51c28cfd69d497569c3b5cfd11c366e27c8984e1
CuzImAzizx <100154535+CuzImAzizx@users.noreply.github.com>
2025-07-30 12:52:55 +0300

    Add saving members and channels in data/*.json

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import discord
 from dotenv import load_dotenv
 import os
+import json
 # ==== End of imports ====
 
 # ==== Start of bot's logic ====
@@ -14,7 +15,43 @@ class MyClient(discord.Client):
             print(f"The bot have joined {client.guilds.__len__()} guilds. Multiple guilds logging is not supported")
             client.close()
 
-        # TODO: Store the guild, members, and all dat shit.
+        # Save all members
+        print("Saving all members...")
+        all_members_gen = client.get_all_members()
+        all_members = []
+        for member in all_members_gen:
+            member_obj = {
+                "id": member.id,
+                "user_name": member.name,
+                "global_name": member.global_name,
+                # "avatar_url": member.avatar.url, # TODO: Fix this: AttributeError: 'NoneType' object has no attribute 'url'
+                # "banner_url": member.banner.url,
+                "created_at": str(member.created_at)
+            }
+            all_members.append(member_obj)
+        try:
+            with open('data/members.json', 'w', encoding='utf-8') as f:
+                json.dump(all_members, f, ensure_ascii=False, indent=4)
+                print("Members data saved to data/members.json successfully!")
+        except Exception as e:
+            print(f"Error saving members data: {e}")
+        
+        # Save all channels
+        print("Saving all channels...")
+        all_channels_gen = client.get_all_channels()
+        all_channels = []
+        for channel in all_channels_gen:
+            channel_obj = {
+                "id": channel.id,
+                "name": channel.name,
+            }
+            all_channels.append(channel_obj)
+        try:
+            with open('data/channels.json', 'w', encoding='utf-8') as f:
+                json.dump(all_channels, f, ensure_ascii=False, indent=4)
+                print("Channels data saved to data/channels.json successfully!")
+        except Exception as e:
+            print(f"Error saving channels data: {e}")
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
@@ -22,8 +59,7 @@ class MyClient(discord.Client):
 # ==== End of bot's logic ====
 
 # ==== Start of Intents, permissions, and tokens ====
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
 
 client = MyClient(intents=intents)
 load_dotenv()
